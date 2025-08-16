c4496295a0ec90d1befbaeed1fe032ec1fa04de6
ankitRaghuwanshi <ankitraghuwanshi96@gmail.com>
2025-08-16 21:10:08 +0530

    initial

--- mern-2/index.js
+++ mern-2/index.js
@@ -8,18 +8,15 @@ app.use(express.json())
 
 const PORT=3000
 
-let USERS_ARRAY=[
-    {
-        name:"bruce",
-        id:5,
-        address:"gotham"
-    },
-    {
-        name:"batman",
-        id:6,
-        address:"gotham"
-    }
-]
+let USERS_ARRAY = [{
+    userId: 1,
+    name: "Bruce",
+    address: "Gotham"
+}, {
+    userId: 2,
+    name: "Riddler",
+    address: "Gotham"
+}]
 
 //Registering the Routes separately 
 app.get('/',(req,res)=>{
@@ -42,19 +39,51 @@ app.get('/hello',(req,res)=>{
     res.json(json)
 })
 
-app.get('/users',(req,res)=>{
+const authenticateMiddleware=(req,res,next)=>{
+    console.log("middleware 1 is called")
     
+    if(req.headers.password ==="test"){
+        //allow user to proceed
+        next()
+    }else{
+        res.status(500).send("you are not authenticated")
+    }
+}
+//dummy middleware
+const testMiddleware=(req,res,next)=>{
+    console.log("middleware 2 is called")
+    next()
+}
+
+app.get('/users',[authenticateMiddleware, testMiddleware] ,(req,res)=>{
+
+    //automatically set the content header
     res.json(USERS_ARRAY)
+})
+
+//how to get particular user with id
+app.get('/users/:userId',(req,res)=>{
+    console.log(req.params)
+
+    const user=USERS_ARRAY.find((user)=>
+        user.userId === parseInt(req.params.userId, 10)
+    )
+
+    //automatically set the content header
+    res.json(user)
 })
 
 app.post('/add-user',(req,res)=>{
     const formData=req.body
-    console.log(formData)
-    USERS_ARRAY.push({
-        name: formData.name,
-        id: formData.id,
-        address: formData.address
-    })
+    //console.log(formData)
+
+    USERS_ARRAY.push(
+        {
+            userId: formData.userId,
+            name: formData.name,
+            address: formData.address
+        }
+    )
 
     res.json({
         success:true
@@ -65,10 +94,17 @@ app.delete('/delete-user',(req,res)=>{
     const formData=req.body
     
     USERS_ARRAY=USERS_ARRAY.filter((user)=>{
-        return user.id !== formData.id
+        return user.userId !== formData.userId
     })
 
     res.json({success:true})
+})
+
+//for invalid url
+//In build middleware that runs for any route not specified above
+//catch-all route to indicate 404 error
+app.use((req,res)=>{
+    res.status(404).send("PAGE NOT FOUND")
 })
 
 app.listen(PORT ,()=>{

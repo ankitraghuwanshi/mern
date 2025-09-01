7e3f4e63868dc80ca76bccedc6c140d62c288879
ankitRaghuwanshi <ankitraghuwanshi96@gmail.com>
2025-09-01 21:17:29 +0530

    final

--- mern-7-Project-3/backend/middlewares/authMiddleware.js
+++ mern-7-Project-3/backend/middlewares/authMiddleware.js
@@ -1,5 +1,4 @@
-const jwt =require("jsonwebtoken")
-const { JWT_SECRET } = process.env 
+const jwt =require("jsonwebtoken") 
 
 function authMiddleware(req, res, next){
     try {
@@ -7,7 +6,7 @@ function authMiddleware(req, res, next){
         const token = req.headers.authorization.split(" ")[1]
 
         //verify if token is correct or not?
-        const verifiedToken = jwt.verify(token, JWT_SECRET)
+        const verifiedToken = jwt.verify(token, process.env.JWT_SECRET)
 
         //extract the userId from verifiedToken and save it in request
         req.body.userId = verifiedToken.userId

c0ae60bc2dad33ded21561bc5b28eabc7df99ec3
ankitRaghuwanshi <ankitraghuwanshi96@gmail.com>
2025-09-02 19:37:27 +0530

    initial

--- mern-8-Project-4/backend/routes/movieRoute.js
+++ mern-8-Project-4/backend/routes/movieRoute.js
@@ -1,15 +1,17 @@
 const express=require("express")
 const movieRouter=express.Router()
 
+const {authMiddleware} = require("../middlewares/authMiddleware")
+
 const {getAllMovieHandler,updateMovieHandler,addMovieHandler} =require("../controllers/movieController")
 
 //add a movie
-movieRouter.post('/add-movie', addMovieHandler)
+movieRouter.post('/add-movie',authMiddleware, addMovieHandler)
 
 //get all movie
-movieRouter.get('/get-all-movie', getAllMovieHandler)
+movieRouter.get('/get-all-movie',authMiddleware, getAllMovieHandler)
 
 //get all movie
-movieRouter.put('/update-movie', updateMovieHandler)
+movieRouter.put('/update-movie',authMiddleware, updateMovieHandler)
 
 module.exports = movieRouter

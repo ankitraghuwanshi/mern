72481eb9c9adc63bb0081b348bfe074f9fd7561e
ankitRaghuwanshi <ankitraghuwanshi96@gmail.com>
2025-09-04 15:58:39 +0530

    initial

---
+++ mern-9-Project-5/backend/app.js
@@ -0,0 +1,30 @@
+const express = require("express")
+const app =express()
+var cors = require('cors')
+//.env file use
+const dotenv = require('dotenv')
+dotenv.config()
+//process.env contain .env data
+
+const userRouter = require("./routes/userRoute")
+const movieRouter = require("./routes/movieRoute")
+
+// Add in middleware to handle request body as JSON
+app.use(express.json())
+app.use(cors())
+
+//registering root level route for user
+app.use('/api/user', userRouter)
+
+//registering root level route for movie
+app.use('/api/movies', movieRouter)
+
+//DATABASE
+//note:- after dotenv.config() because process.env uses for DB_URL
+const {connectDB}=require("./config/db")
+connectDB()
+
+
+app.listen(process.env.PORT, ()=>{
+    console.log('backend application started')
+})
---
+++ mern-9-Project-5/backend/routes/movieRoute.js
@@ -0,0 +1,17 @@
+const express=require("express")
+const movieRouter=express.Router()
+
+const {authMiddleware} = require("../middlewares/authMiddleware")
+
+const {getAllMovieHandler,updateMovieHandler,addMovieHandler} =require("../controllers/movieController")
+
+//add a movie
+movieRouter.post('/add-movie',authMiddleware, addMovieHandler)
+
+//get all movie
+movieRouter.get('/get-all-movie',authMiddleware, getAllMovieHandler)
+
+//update movie
+movieRouter.put('/update-movie',authMiddleware, updateMovieHandler)
+
+module.exports = movieRouter

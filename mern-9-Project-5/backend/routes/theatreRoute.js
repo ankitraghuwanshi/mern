def4976882f4b1830523f50b75e909ce7f8cefc6
ankitRaghuwanshi <ankitraghuwanshi96@gmail.com>
2025-09-05 18:42:13 +0530

    initial

--- mern-9-Project-5/backend/routes/theatreRoute.js
+++ mern-9-Project-5/backend/routes/theatreRoute.js
@@ -58,7 +58,7 @@ theatreRouter.get("/get-all-theatres", async (req, res) => {
     try {
         // This will by default omit all reference fields
         // When we populate the owner, EVERY field from Owner is included
-        const allTheatres = await TheatreModel.find().populate("owner")
+        const allTheatres = await Theatre.find().populate("owner")
 
         res.send({
             success: true,
@@ -74,13 +74,13 @@ theatreRouter.get("/get-all-theatres", async (req, res) => {
 })
 
 // Getting all theatres for a particular user ID as owner
-theatreRouter.get("/get-all-theatres-by-owner", async (req, res) => {
+theatreRouter.get("/get-all-theatres-by-owner/:ownerID", async (req, res) => {
     try {
-        const allTheatresByOwner = await TheatreModel.find({
-            owner: req.body.owner
+        const allTheatresByOwner = await Theatre.find({
+            owner: req.params.ownerID
         })
 
-        //console.log(req.params.ownerID, allTheatresByOwner)
+        console.log(req.params.ownerID, allTheatresByOwner)
 
         res.send({
             success: true,
@@ -94,6 +94,5 @@ theatreRouter.get("/get-all-theatres-by-owner", async (req, res) => {
         })
     }
 })
-
 
 module.exports = theatreRouter
